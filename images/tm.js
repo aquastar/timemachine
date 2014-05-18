f13690139984ad205803ef4f9dfafa7ab1df1905
aquastar <860825@gh>
2014-05-18 09:04:21 +0800

    task list

---
+++ images/tm.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Danny on 2014/5/17.
+ */
+
+function allowDrop(ev)
+{
+    ev.preventDefault();
+}
+
+function drag(ev)
+{
+    ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev)
+{
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
